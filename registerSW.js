8f19d42483469004d76bc1ca6cbb2be8b5ebbe52
Venubalan T <venu.balan@qubecinema.com>
2025-03-09 09:37:21 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/demo-app/sw.js', { scope: '/demo-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
